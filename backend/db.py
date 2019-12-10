50c2c56c1d0e59c8cfb0021e33bf69059814034e
Dzan Operta <dzanoperta@yahoo.com>
2019-12-11 00:06:26 +0100

    commenting database py

--- backend/db.py
+++ backend/db.py
@@ -2,22 +2,23 @@ import sqlite3
 import csv
 import datetime
 
-
+# Method to transform date format
 def transform(date):
     return datetime.datetime.strptime(date.strip(), "%m/%d/%Y").strftime("%Y/%m/%d")
 
 
 if __name__ == '__main__':
-    # def populate():
     con = sqlite3.connect(r"pythonsqlite.db")
     cur = con.cursor()
+
+    # Create data table
     cur.execute("CREATE TABLE data (Datum TEXT, StationNumber REAL, Latitude REAL, Longitude REAL, Depth REAL, Fluorescence REAL,"
                 " CalculatedChlorophyll REAL, OxygenElectrodeOutput REAL, OxygenSaturation REAL, CalculatedOxygen REAL,"
                 " OpticalBackscatter REAL, CalculatedSPM REAL, Salinity REAL, Temperature REAL, SigmaT REAL);")  # use your column names here
 
-    with open('data.csv') as fin:  # `with` statement available in 2.5+
-        # csv.DictReader uses first line in file for column headings by default
-        dr = csv.DictReader(fin)  # comma is default delimiter
+    # Open data.csv and insert it into data table
+    with open('data.csv') as fin:
+        dr = csv.DictReader(fin)
         to_db = [(transform(i['Date']), i['Station Number'], i['lat'], i['long'], i['Depth'], i['Fluorescence'],
                   i['Calculated Chlorophyll'], i['Oxygen Electrode Output'], i['Oxygen Saturation %'],
                   i['Calculated Oxygen'], i['Optical Backscatter'], i['Calculated SPM'], i['Salinity'],
@@ -28,11 +29,12 @@ if __name__ == '__main__':
                     " OpticalBackscatter, CalculatedSPM, Salinity, Temperature, SigmaT)"
                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
 
+    #Create stations table
     cur.execute("CREATE TABLE stations (StationNumber REAL, Latitude REAL, Longitude REAL);")
 
-    with open('sngeo.csv') as fin:  # `with` statement available in 2.5+
-        # csv.DictReader uses first line in file for column headings by default
-        dr = csv.DictReader(fin)  # comma is default delimiter
+    # Open sngeo.csv and insert it into stations table
+    with open('sngeo.csv') as fin:
+        dr = csv.DictReader(fin)
         to_db = [(i['num'], i['latitude'], i['longitude']) for i in dr]
 
     cur.executemany("INSERT INTO stations (StationNumber, Latitude, Longitude)"
